2eb9ac6e421e83116fcc84c7542b9b2160195364
sammko <samko.cavoj@gmail.com>
2013-02-08 15:58:35 +0100

    Initial upload.

---
+++ ArduinoController/Arduino.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArduinoController
+{
+    class Arduino
+    {
+        private string ComPort;
+        private string DeviceVersion;
+        private string DeviceName;
+        private byte PHandlerID;
+        private int Baud;
+        private SerialPort serial;
+        private bool isInit;
+
+        //ReadOnly Properties
+        public string getCOM() { return this.ComPort; }
+        public string getVersion() { return this.DeviceVersion; }
+        public string getName() { return this.DeviceName; }
+        public byte getPacketHandlerID() { return this.PHandlerID; }
+        public int getBaudRate() { return this.Baud; }
+        public bool getIsInit() { return this.isInit; }
+        public int getInitPacketLength() { return 2 + DeviceName.Length; }
+        public int getAvailableLength() { return serial.BytesToRead; }
+        //Constructor & Initialization
+        public Arduino(string COM, string DeviceVersion = "Arduino", string DeviceName = "My Arduino", byte PHandlerID = 0, int Baud = 9600)
+        {
+            this.ComPort = COM;
+            this.DeviceVersion = DeviceVersion;
+            this.DeviceName = DeviceName;
+            this.PHandlerID = PHandlerID;
+            this.Baud = Baud;
+            initSerial();
+            sendInitPacket();
+        }
+
+        private void initSerial()
+        {
+            serial = new SerialPort(this.ComPort, this.Baud);
+            serial.Open();
+        }
+
+        public void killSerial()
+        {
+            serial.Close();
+        }
+
+        private void sendInitPacket()
+        {
+            
+            byte[] data = new byte[2 + DeviceName.Length];
+            data[0] = 0x2F;
+            data[1] = getPacketHandlerID();
+            for (int i = 0; i < DeviceName.Length - 1; i++)
+            {
+                data[i + 2] = (byte) DeviceName[i];
+            }
+            serial.Write(data, 0, data.Length);
+            isInit = true;
+        }
+
+        public void killDevice()
+        {
+            byte[] data = new byte[1] { 0x3F };
+            this.sendPacket(data);
+        }
+
+        public void reviveDevice()
+        {
+            byte[] data = new byte[1] { 0x4F };
+            this.sendPacket(data);
+        }
+
+        public void sendPacket(byte[] data)
+        {
+            serial.Write(data, 0, data.Length);
+        }
+
+        public string readString(int length = 0)
+        {
+            if (length == 0) { length = this.getAvailableLength(); }
+            return System.Text.Encoding.UTF8.GetString(readPacket(length));
+        }
+
+
+        public byte[] readPacket(int length = 0)
+        {
+            if (length == 0) { length = this.getAvailableLength(); }
+            byte[] buffer = new byte[length];
+            serial.Read(buffer, 0, length);
+            return buffer;
+        }
+    }
+}
---
+++ ArduinoController/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ArduinoController
+{
+    class Program
+    {
+        static Arduino ard;
+        static void Main(string[] args)
+        {
+            string com;
+            if (args.Length == 0)
+            {
+                Console.Write("Enter COM port (Press Enter for COM3): ");
+                com = Console.ReadLine();
+                if (com == "") com = "COM3";
+            }
+            else
+            {
+                com = args[0];
+            }
+            ard = new Arduino(COM: com, DeviceVersion: "Arduino Uno Rev.3", DeviceName: "SammkosArduino");
+            
+            while (true)
+            {
+                if (ard.getAvailableLength() > 0)
+                {
+                    Console.Write(ard.readString());
+                }
+            }
+        }
+    }
+}
